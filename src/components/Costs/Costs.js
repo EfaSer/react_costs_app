6c2a13db096df953435aaf7e74c78c6e6393ac98
Sergey Efanov <super.sirigo@yandex.ru>
2023-09-13 22:33:55 +0300

    commit

--- src/components/Costs/Costs.js
+++ src/components/Costs/Costs.js
@@ -1,42 +1,27 @@
 import "./Costs.css";
 import { CostList } from "./CostList";
 import { CostFilter } from "./CostFilter";
+import { CostsDiagram } from "./CostsDiagram";
 import { Card } from "../UI/Card.js";
 import { useState } from "react";
 
 export const Costs = (props) => {
-    const [year, setYear] = useState("2021");
-    const getYear = (selectYear) => {
-        setYear(selectYear);
-    };
+  const [year, setYear] = useState("2021");
+  const getYear = (selectYear) => {
+    setYear(selectYear);
+  };
 
-    const filterCosts = props.costs.filter((cost) => {
-        return cost.date.getFullYear().toString() === year;
-    });
+  const filterCosts = props.costs.filter((cost) => {
+    return cost.date.getFullYear().toString() === year;
+  });
 
-    return (
-        <div>
-            <Card className="costs">
-                <CostFilter onGetYear={getYear} year={year} />
-                <CostList costs={filterCosts} year={year} />
-
-                {/* статический вариант */}
-                {/*<CostItem
-                    date={props.costs[0].date}
-                    description={props.costs[0].description}
-                    amount={props.costs[0].amount}
-                />
-                <CostItem
-                    date={props.costs[1].date}
-                    description={props.costs[1].description}
-                    amount={props.costs[1].amount}
-                />
-                <CostItem
-                    date={props.costs[2].date}
-                    description={props.costs[2].description}
-                    amount={props.costs[2].amount}
-                />*/}
-            </Card>
-        </div>
-    );
+  return (
+    <div>
+      <Card className="costs">
+        <CostFilter onGetYear={getYear} year={year} />
+        <CostsDiagram costs={filterCosts} />
+        <CostList costs={filterCosts} year={year} />
+      </Card>
+    </div>
+  );
 };
---
+++ src/components/Diagram/Diagram.js
@@ -0,0 +1,21 @@
+import { DiagramBar } from "./DiagramBar";
+import "./Diagram.css";
+
+export const Diagram = (props) => {
+  const dataSetsValues = props.dataSets.map((dataSet) => dataSet.value);
+
+  const maxMonthCost = Math.max(...dataSetsValues);
+
+  return (
+    <div className="diagram">
+      {props.dataSets.map((dataSet) => (
+        <DiagramBar
+          key={dataSet.label}
+          value={dataSet.value}
+          maxValue={maxMonthCost}
+          label={dataSet.label}
+        />
+      ))}
+    </div>
+  );
+};
